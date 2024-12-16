17e3a471e8245b1f6ad731370bab1621524111c5
Owen Bean <owenbean400@gmail.com>
2024-12-16 13:52:37 -0500

    Cleanup upload helper

--- lib/fastlane/plugin/google_data_safety/helper/upload_google_data_safety_helper.rb
+++ lib/fastlane/plugin/google_data_safety/helper/upload_google_data_safety_helper.rb
@@ -5,12 +5,6 @@ module Fastlane
 
   module Helper
     class GoogleDataSafetyHelper
-      # class methods that you define here become available in your action
-      # as `Helper::DataSafetyHelper.your_method`
-      #
-      def self.show_message
-        UI.message("Hello from the data_safety plugin helper!")
-      end
     end
   end
 end
